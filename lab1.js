a8fde515d5fef21745a0aae5034ebd56f1c66b14
Mohith <mohithjegan@gmail.com>
2024-09-12 21:18:08 -0400

    New files and changes are added

---
+++ lab1.js
@@ -0,0 +1,8 @@
+//alert("Hello World!"); Added an inline comment to  prevent it from running without deleting it
+var myName = "Mohith"; //NUMBER data type
+var age = 25; // NUMBER data type
+var numberOfDaysInYear = 365; // NUMBER data type
+var totalOfMyAge = age* numberOfDaysInYear;
+var phrase = " days old… more or less.";
+//alert(totalOfMyAge); Added an inline comment to  prevent it from running without deleting it
+alert(myName + " is "+totalOfMyAge+phrase);
